39ac7ec5cd7f10d8d91b0c75aabb9b34ccdd5c0f
Lokeyli <zcablie@ucl.ac.uk>
2023-03-21 04:38:22 +0000

    Draft Diagram v2

--- client/app/diagram/page.tsx
+++ client/app/diagram/page.tsx
@@ -7,22 +7,7 @@ import { useState } from "react";
 import { Chart as ChartJS, registerables } from 'chart.js';
 ChartJS.register(...registerables);
 import { Line, Scatter, Chart } from "react-chartjs-2";
-
-// type result = {
-//     labelsX: string[],
-//     labelsY: string[],
-//     datasets: {
-//         data: number[],
-//         correlation: number,
-//     }
-// }
-
-// const curDisplay: result = {
-//     labelsX: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-//     labelsY: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-//     datasets: {
-        
-// }
+import { Dropdown } from "@nextui-org/react";
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
@@ -46,7 +31,7 @@ const result = {
       borderWidth: 2,
     },
     {
-      type: 'bar' as const,
+      type: 'scatter' as const,
       label: 'Dataset 3',
       backgroundColor: 'rgb(53, 162, 235)',
       data: [33, 53, 85, 41, 44, 65],
@@ -54,17 +39,52 @@ const result = {
   ],
 };
 
-function RenderCorrelation(){
+function RenderDiagram(){
     return(
-        // Display a page shows the title "Diagram" and a diagram
-        <div className="fixed left-0 top-0 bg-slate-300 w-screen h-screen sm:ml-14">
-            <div className="h-14 bg-slate-50 ">{/* Header Div */}</div>
-            <h1 className="text-3xl">Diagram</h1>
-            <div className="App">
-            <Chart data={result} />
+      // Display a page shows the title "Diagram" and a diagram
+
+      <div className="fixed left-0 top-0 bg-slate-300 w-screen h-screen sm:ml-14">
+          <div className="h-14 bg-slate-50 ">
+            <div className="flex flex-row">
+              <h1 className="text-3xl">X-axis</h1>
+              <Dropdown>
+                <Dropdown.Button flat> Select Table for X </Dropdown.Button>
+                <Dropdown.Menu aria-label="Static Actions">
+                  <Dropdown.Item key="new"> A </Dropdown.Item>
+                  <Dropdown.Item key="copy"> B </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Dropdown>
+                <Dropdown.Button flat> Select Column for X </Dropdown.Button>
+                <Dropdown.Menu aria-label="Static Actions">
+                  <Dropdown.Item key="new"> A </Dropdown.Item>
+                  <Dropdown.Item key="copy"> B </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <h1 className="text-3xl">Y-axis</h1>
+              <Dropdown>
+                <Dropdown.Button flat> Select Column for Y </Dropdown.Button>
+                <Dropdown.Menu aria-label="Static Actions">
+                  <Dropdown.Item key="new"> A </Dropdown.Item>
+                  <Dropdown.Item key="copy"> B </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Dropdown>
+                <Dropdown.Button flat> Select Column for Y </Dropdown.Button>
+                <Dropdown.Menu aria-label="Static Actions">
+                  <Dropdown.Item key="new"> A </Dropdown.Item>
+                  <Dropdown.Item key="copy"> B </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
-        </div>
+          </div>
+          <h1 className="text-2xl">Diagram</h1>
+          <div className="App">
+          <Chart data={result} />
+          </div>
+      </div>
     );
 }
 
-export default RenderCorrelation;
+export default RenderDiagram;
